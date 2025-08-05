8ef51d13eab67ac2a54ac6fe2980df40863efca3
Audrey Phung <audreyphung8@gmail.com>
2025-08-05 14:10:49 -0700

    Started on streaks

---
+++ src/components/Streaks.js
@@ -0,0 +1,23 @@
+import { supabase } from '../utils/hooks/supabase'
+
+export async function fetchStreakInfo(roomId) 
+{
+    const { data, error } = await supabase
+        .from("track_streaks")
+        //Fetch the time and current streak info
+        .select('updated, current_streak')
+        //Filter by user_id
+        .eq('user_id', roomId)
+        .single()
+
+        console.log("Fetched streak data:", data);
+        console.log("Supabase error:", error);
+
+
+    if (error) 
+    {
+        throw error
+    }
+    return data
+}
+
